364ac8298ddae2c17a87500b5b0c4d1ddc025fdf
subadevaeswaran <124487800+subadevaeswaran@users.noreply.github.com>
2023-03-29 11:26:59 +0530

    Add files via upload

---
+++ Drum Kit Using DOM/main/index.js
@@ -0,0 +1,43 @@
+alert("heyyy")
+var number=document.querySelectorAll(".drum").length;
+for(i=0;i<number;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+        var btn=this.innerHTML
+        switch(btn){
+            case "w":
+                var crash = new Audio("sounds/crash.mp3");
+                crash.play();
+                break;
+            case "a":
+                var kick = new Audio("sounds/kick.mp3");
+                kick.play();
+                break;
+            case "s":
+                var snare = new Audio("sounds/snare.mp3");
+                snare.play();    
+                break;
+            case "d":
+                var tom1 = new Audio("sounds/tom-1.mp3");
+                tom1.play();   
+                break;
+            case "j":
+                var tom2= new Audio("sounds/tom-2.mp3");
+                tom2.play();
+                break;
+            case "k":
+                var tom3 = new Audio("sounds/tom-3.mp3");
+                tom3.play();   
+                break;
+            case "l":
+                var tom4 = new Audio("sounds/tom-4.mp3");
+                tom4.play();
+                break;
+            default:
+                console.log()   
+             
+
+
+        }
+ });
+}
